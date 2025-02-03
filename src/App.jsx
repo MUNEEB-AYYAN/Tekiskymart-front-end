13784ced2c4670ac2cf0215cd3eccf497835aab1
MUNEEB-AYYAN <muneebayyan348@gmail.com>
2025-02-03 16:20:30 +0530

    tt

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
     
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="h-screen bg-gray-100 mt-44">
       <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind with Vite!</h1>
-      <h1 className="text-4xl bg-red-700 font-bold text-blue-600">Hello, Tailwind with Vite!</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind with Vite!</h1>
     </div>  
     </>
   )
