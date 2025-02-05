45d1b3abfa0d5a43aebeba132ddc1a797b2db33a
unknown <zk271278@gmail.com>
2025-02-05 16:24:36 +0530

    compin

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './components/Home'
 import Carousel from './components/carousel'
 import Carousel2 from './components/Carousel2'
 import ProductCard from './components/ProductCard'
+import Gift from './components/Gift'
+// import Dates from './components/Dates'
 
 
 
@@ -12,13 +14,18 @@ function App() {
   return (
     <div>
       {/* <h1 className='bg-red-600 text-white text-3xl'>Hello</h1> */}
-      <Home></Home>
+      {/* <Home></Home>
       <br></br>
       <Carousel></Carousel>
       <br></br>
       <Carousel2></Carousel2>
       <br></br>
-      <ProductCard></ProductCard>
+      <ProductCard></ProductCard> 
+      <br /> */}
+       {/* <Gift></Gift>
+      <br></br> */}
+      {/* <Dates></Dates> */}
+      <br />
     
       
       
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { div } from "framer-motion/client";
 import React from "react";
 
 const products = [
@@ -29,21 +30,70 @@ const products = [
     oldPrice: 390,
     image: "https://tekisky-mart.s3.ap-south-1.amazonaws.com/images/1707635962153_1.jpg",
   },
+  {
+    id: 4,
+    name: "Charpai Set of 3-Wooden and Jute Miniature Charpai Cot Set of 3 Toy for Kids.",
+    price: 299,
+    oldPrice: 500,
+    image: "https://tekisky-mart.s3.ap-south-1.amazonaws.com/images/1707635715171_3.jpg",
+  },
+  {
+    id: 5,
+    name: "Jewelry Box 4x4-Wooden Jewelry Box for Women Jewel Organizer Square Elephant Décor, 4 x 4 inches",
+    price: 299,
+    oldPrice: 500,
+    image: "https://tekisky-mart.s3.ap-south-1.amazonaws.com/images/1707593531859_2.jpg",
+  },
+  {
+    id: 6,
+    name: "Agarbatti Dhoop Wooden Incense Holder | Agarbatti Stand | Dhoop Stand.",
+    price: 120,
+    oldPrice: 320,
+    image: "https://tekisky-mart.s3.ap-south-1.amazonaws.com/images/1707593422006_1.jpg",
+  },
+  {
+    id: 7,
+    name: "Agarbatti Case Karigar Creations Sheesham Wooden and Brass Agarbati Holder.",
+    price: 120,
+    oldPrice: 320,
+    image: "https://tekisky-mart.s3.ap-south-1.amazonaws.com/images/1707593332603_1.jpg",
+  },
+  {
+    id: 8,
+    name: "Door Bell Door Bells Ornaments Hanging, Decoration Pendant for",
+    price: 200,
+    oldPrice: 400,
+    image: "https://tekisky-mart.s3.ap-south-1.amazonaws.com/images/1707593107771_2.jpg",
+  },
+  {
+    id: 9,
+    name: "Ecopretier Massager Medium Wooden Handheld Foot Roller Acupressure Massager Reflexology Tools.",
+    price: 200,
+    oldPrice: 400,
+    image: "https://tekisky-mart.s3.ap-south-1.amazonaws.com/images/1707592966404_2.jpg",
+  }
+  
+  
 ];
+
 
 const ProductCard = ({ product }) => {
   return (
+  <div>
+
     <div className="bg-white shadow-md rounded-lg p-3 w-medium sm:w-60">
       <img
         src={product.image}
         alt={product.name}
         className="w-45 h-50 object-cover rounded-md"
-      />
+        />
+        
       <h3 className="text-sm font-semibold mt-2 truncate">{product.name}</h3>
       <div className="flex items-center gap-2 mt-1">
         <span className="text-red-600 font-bold">₹{product.price}</span>
         <span className="line-through text-gray-400 text-sm">₹{product.oldPrice}</span>
       </div>
+    </div>
     </div>
   );
 };
@@ -52,7 +102,7 @@ const ProductGrid = () => {
   return (
     <div className="p-5">
       <h2 className="text-xl font-bold text-center mb-5">GIFT ITEMS</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="flex flex-wrap justify-center gap-4 ml-4 ">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
